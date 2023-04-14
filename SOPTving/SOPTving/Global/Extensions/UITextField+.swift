3009a844dba7ca4fd1fae3c9904196a57e380075
Kim Minjae <ffalswo2@naver.com>
2023-04-15 01:13:52 +0900

    [Add] #3 - 파일들 추가

---
+++ SOPTving/SOPTving/Global/Extensions/UITextField+.swift
@@ -0,0 +1,8 @@
+//
+//  UITextField+.swift
+//  SOPTving
+//
+//  Created by 김민재 on 2023/04/14.
+//
+
+import Foundation
---
+++ SOPTving/SOPTving/Global/UIComponents/AuthTextField.swift
@@ -0,0 +1,20 @@
+//
+//  AuthTextField.swift
+//  SOPTving
+//
+//  Created by 김민재 on 2023/04/14.
+//
+
+import UIKit
+
+class AuthTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
