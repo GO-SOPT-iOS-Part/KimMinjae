205dba30ac5667295b20cc76998147c652e8b946
Kim Minjae <ffalswo2@naver.com>
2023-04-15 01:14:11 +0900

    [Feat] #3 - AuthTextField 구현

--- SOPTving/SOPTving/Global/Extensions/UIView+.swift
+++ SOPTving/SOPTving/Global/Extensions/UIView+.swift
@@ -5,4 +5,24 @@
 //  Created by 김민재 on 2023/04/14.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        views.forEach { self.addSubview($0) }
+    }
+
+    /// UIView 의 모서리가 둥근 정도를 설정하는 메서드
+    func makeRounded(cornerRadius: CGFloat?) {
+        if let cornerRadius = cornerRadius {
+            self.layer.cornerRadius = cornerRadius
+        } else {
+            // cornerRadius 가 nil 일 경우의 default
+            self.layer.cornerRadius = self.layer.frame.height / 2
+        }
+
+        self.clipsToBounds = true
+//        self.layer.masksToBounds = false
+    }
+}
