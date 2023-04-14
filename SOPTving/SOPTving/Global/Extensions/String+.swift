691df7e3468787f9bd64da06e2fd7001006b650c
Kim Minjae <ffalswo2@naver.com>
2023-04-15 03:36:06 +0900

    [Feat] #3 - Extension 추가

---
+++ SOPTving/SOPTving/Global/Extensions/String+.swift
@@ -0,0 +1,17 @@
+//
+//  String+.swift
+//  SOPTving
+//
+//  Created by 김민재 on 2023/04/15.
+//
+
+import Foundation
+
+
+extension String {
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
---
+++ SOPTving/SOPTving/Global/Extensions/UIButton+.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton+.swift
+//  SOPTving
+//
+//  Created by 김민재 on 2023/04/15.
+//
+
+import UIKit
+
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
